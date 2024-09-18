05a9213e15a31a3998476e2c38183cfb219ab58a
PIYUSH-MISTRY69 <piyushmistry12300@gmail.com>
2024-09-18 16:27:20 +0530

    changes made

--- script2.js
+++ script2.js
@@ -33,3 +33,40 @@ let newwbut=document.querySelector("#menu1");
       dots[slideIndex-1].className += " active";
       setTimeout(showSlides, 3500); // Change image every 2 seconds
     }
+
+    function func() {
+      // Declare variables
+      var input, filter, ul, li, a, i, txtValue;
+      input = document.getElementById('myInput');
+      filter = input.value.toUpperCase();
+      ul = document.getElementById("myUL");
+      li = ul.getElementsByTagName('li');
+    
+      // Loop through all list items, and hide those who don't match the search query
+      for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          li[i].style.display = "";
+        } else {
+          li[i].style.display = "none";
+        }
+      }
+    }
+
+
+let k=0;
+const ul=document.getElementById('myUL');
+input = document.getElementById('myInput');
+input.addEventListener("click",()=>
+{
+  if(k===0)
+    {
+      ul.classList.remove("unseen");
+      k=1;
+    }
+    else{
+      ul.classList.add("unseen");
+      k=0;
+    }
+})
